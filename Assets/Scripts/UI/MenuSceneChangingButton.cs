e73a0287691ab590031416ebbbf7cc65c874987f
@vladk4210 <vladk4210@gmail.com>
2024-04-24 12:12:16 +0300

    /

--- Assets/Scripts/UI/MenuSceneChangingButton.cs
+++ Assets/Scripts/UI/MenuSceneChangingButton.cs
@@ -7,7 +7,8 @@ namespace UI
 {
     public class MenuSceneChangingButton : MonoBehaviour
     {
-        private const string _sceneName = "Level_1"; //Level_TestScene Level_1 Level_MovementTest
+        private const string _sceneName = "Level_UITestScene";
+        //Level_TestScene Level_1 Level_MovementTest Level_UITestScene
         private ICharacterSelector _characterSelector;
 
 
