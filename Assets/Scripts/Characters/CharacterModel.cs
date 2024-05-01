9dfd2130913a773406b0401be33437796d6877d4
@vladk4210 <vladk4210@gmail.com>
2024-05-01 16:02:26 +0300

    Done prefabs for abilityes

--- Assets/Scripts/Characters/CharacterModel.cs
+++ Assets/Scripts/Characters/CharacterModel.cs
@@ -1,10 +1,12 @@
 using CharactersStats;
 using System;
+using System.Collections.Generic;
 
 [Serializable]
 public class CharacterModel : CharacterModelBase
 {
     public CharacterType Type;
+    public List<InteractionType> Abilities;
 
     private OriginStats _originStats;
 
@@ -19,6 +21,12 @@ public class CharacterModel : CharacterModelBase
         Speed = _speed;
         LaunchPower = _launchPower;
         Velocity = _velocity;
+
+        Abilities = new List<InteractionType>
+        {
+            InteractionType.BasicAttack,
+            InteractionType.Knight_HeavyAttack
+        };
     }
 }
 
