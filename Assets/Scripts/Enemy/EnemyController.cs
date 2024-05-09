47d9705d491fb7c79ef70dbf2cb72fb3d6942a45
Slava <ivancukvaceslav5@gmail.com>
2024-05-09 15:25:32 +0300

    enemy skill panel could be shown when unactive

--- Assets/Scripts/Enemy/EnemyController.cs
+++ Assets/Scripts/Enemy/EnemyController.cs
@@ -128,7 +128,10 @@ namespace Enemy
         public void OnClick(Transform point, PointerEventData eventData)
         {
             _slingShotInitPosition = point;
-            _uIViewmodel.ActivateSkillsBTNs();
+            if (!IsActive)
+            {
+                _uIViewmodel.ActivateSkillsBTNs();
+            }
 
             //Debug.Log($"-----|{CharacterModel.Type}|-----");
             //Debug.Log("<color=#189C0C>" + "Hp: " + ModifiableStats.Health.Value + "</color>");
