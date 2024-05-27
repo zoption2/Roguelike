7ab63309d85f7ce61dfd2fb736e28b2b0968acc2
Slava <ivancukvaceslav5@gmail.com>
2024-05-27 15:21:03 +0300

    Fixed spring wall (the orange one)

--- Assets/Scripts/Obstacles/Walls/ReflectionWall.cs
+++ Assets/Scripts/Obstacles/Walls/ReflectionWall.cs
@@ -9,10 +9,8 @@ namespace Obstacles
     {
         public void ProcessCollision(Collision collision, Rigidbody rigidbody, Vector3 velocity)
         {
-            //rigidbody.velocity = Vector3.zero;
-            var modifiedVelocity = velocity * -1.5f;
-            //rigidbody.velocity = Vector3.zero;
-            rigidbody.AddForce(modifiedVelocity, ForceMode.VelocityChange);
+            Vector3 modifiedVelocity = velocity * -1.5f;
+            rigidbody.velocity = modifiedVelocity;
         }
     }
 }
